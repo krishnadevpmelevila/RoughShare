3105b69b3ee3937bae32c44abf7630e1f0757abd
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2021-05-09 17:52:11 +0530

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
+// cors
+const corsOptions = {
+	orgin:process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static('./public'))
 app.use(express.json());
+
 // Routes
 app.use('/',require('./routes/download'));
 app.use('/api/files', require('./routes/files'));
