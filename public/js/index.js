c914ccc9ba5112443bdd8d2d3da69048a460b016
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2021-05-09 18:55:57 +0530

    first commit

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://roughshare.herokuapp.com/";
+const baseURL = "https://roughshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
