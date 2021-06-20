8f6d706838f27125fcbe82fda8d6ba26036f627a
Krishnadev P Melevila <66047990+krishnadevpmelevila@users.noreply.github.com>
2021-06-20 16:47:58 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -1,17 +1,1 @@
-const router = require('express').Router();
-const File = require('../models/file');
-router.get('/:uuid',async (req,res)=>{
-	const file = await File.findOne({uuid:req.params.uuid})
-	if(!file){
-		return res.render('download', { error: 'Link expired or incorrect URL' });
-	};
-	const filePath = `${__dirname}/../${file.path}`;
-	console.log(filepath)
-	console.log("rrrrrrrrrrr")
-	res.download(filePath);
-})
-router.get('/',(req, res)=>{
-	res.render('upload')
-})
 
-module.exports = router;
