5ccd08617e80bbb642e9c94fc4b39cf80b63f400
Krishnadev P Melevila <krishnadevpmelevila@gmail.com>
2021-06-20 18:51:21 +0530

    Final Out

--- server.js
+++ server.js
@@ -1,38 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
-app.use(express.static('public'));
-
+const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
+// cors
+const corsOptions = {
+	orgin:process.env.ALLOWED_CLIENTS.split(',')
+}
 
-app.use(express.json());
-
+app.use(cors(corsOptions));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+app.use(express.static('./public'))
+app.use(express.json());
 
-// Routes 
+// Routes
+app.use('/',require('./routes/download'));
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
+app.use('/files/download',require('./routes/download'));
 
-
-app.listen(PORT, console.log(`Listening on port ${PORT}.`));
+app.listen(PORT, () => {
+	console.log(`i am working on port ${PORT} da!`);
+});
